078b31411af28ee2e404c07fffd223f2a57e5a89
Dedei-AB <andreiabners@gmail.com>
2026-04-06 11:52:10 -0300

    Organização do backend em rotas

---
+++ Primeiro_Backend/src/server.js
@@ -0,0 +1,33 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { tarefaRoutes } from "./tarefa.routes.js";
+
+const server = Fastify();
+const PORT = 3000;
+
+// Habilita o CORS para permitir requisições do Frontend
+server.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+});
+
+server.register(tarefaRoutes, { prefix: "/tarefas" });
+
+server.setNotFoundHandler((request, reply) => {
+  reply.code(404).send({
+    status: "error",
+    message: "O recurso solicitado não existe nesta API.",
+  });
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: PORT });
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  } catch (erro) {
+    console.error(erro);
+    process.exit(1);
+  }
+};
+
+start();
